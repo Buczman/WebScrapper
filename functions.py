114a260a657093c1716b87fce54f09c6b342172b
Buczman <mateusz.buczynski@interia.pl>
2018-05-19 16:06:23 +0200

    functions modifications

--- functions.py
+++ functions.py
@@ -32,23 +32,21 @@ def pLoggerInit(filename = 'log.txt'):
 	if os.path.isfile(filename):
 		os.remove(filename)
 
-def driverdownload(filename,url):
-	time.sleep(5)
+def driverdownload(driver,filename,url):
 	driver.get(url)
 	page = driver.page_source
-	file_ = open(filename, 'w')
+	file_ = open(filename, 'w+')
 	file_.write(page)
 	file_.close()
 	pass
 
 def htmldownload(filename,url):
-	time.sleep(5)
 	req=urllib.request.Request(url, None, {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; G518Rco3Yp0uLV40Lcc9hAzC1BOROTJADjicLjOmlr4=) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3','Accept-Encoding': 'gzip, deflate, sdch','Accept-Language': 'en-US,en;q=0.8','Connection': 'keep-alive'})
 	cookieJar = CookieJar()
 	opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookieJar))
 	response = opener.open(req)
 	rawResponse = response.read().decode('utf8', errors='ignore')
-	with open(filename, 'w', encoding='utf-8') as fid:
+	with open(filename, 'w+', encoding='utf-8') as fid:
 		fid.write(rawResponse)
 	response.close()
 	pass
